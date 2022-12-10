4f4eb49454eda392a829d986cb0ae457aa13e314
tarurata <saycheese5963@gmail.com>
2022-12-11 04:47:18 +0900

    ADdjust new list view to do model validation

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # How to use Functional test
 ## TEST with specific server url
 python manage.py test functional_tests --testrunner="functional_tests.tests.TestRunner" --url="http://xxx.xxx"
-* refar to https://stackoverflow.com/questions/28229864/django-manage-py-is-it-possible-to-pass-command-line-argument-for-unit-testin
+* refer to https://stackoverflow.com/questions/28229864/django-manage-py-is-it-possible-to-pass-command-line-argument-for-unit-testin
 
 ## Test 
 python manage.py test functional_tests
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -7,6 +7,7 @@ from django.http import HttpRequest
 from lists.views import home_page
 from django.template.loader import render_to_string
 from lists.models import Item, List
+from django.utils.html import escape
 
 
 class HomePageTest(TestCase):
@@ -87,3 +88,18 @@ class NewListTest(TestCase):
         correct_list = List.objects.create()
         response = self.client.get(f'/lists/{correct_list.id}/')
         self.assertEqual(response.context['list'], correct_list)
+
+    def test_validation_errors_ar_sent_back_to_home_page_template(self):
+        response = self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        # expected_error = "You can't have an empty list item"
+        expected_error = escape("You can't have an empty list item")
+        # print(response.content.decode())
+        self.assertContains(response, expected_error)
+
+    def test_invalid_list_items_arent_saved(self):
+        self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(List.objects.count(), 0)
+        self.assertEqual(Item.objects.count(), 0)
+
