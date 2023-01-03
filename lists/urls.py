24e89f3c069eb86ca94a22817db36b6e4a5e8a96
tarurata <saycheese5963@gmail.com>
2023-01-03 16:28:19 +0900

    url, placeholder view, and first-cut templates for my_lists

--- lists/urls.py
+++ lists/urls.py
@@ -4,4 +4,5 @@ from lists import views
 urlpatterns = [
     re_path(r'^(\d+)/$', views.view_list, name='view_list'),
     re_path(r'^new$', views.new_list, name='new_list'),
+    re_path(r'^users/(.+)/$', views.my_lists, name='my_lists'),
 ]
